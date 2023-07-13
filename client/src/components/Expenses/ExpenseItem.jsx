c60b29a7bf01616288d753ae6b15780c8696e1c5
Matthew Garry <mhgarry92@gmail.com>
2023-07-13 16:12:20 -0400

    update ExpenseItem component

--- client/src/components/Expenses/ExpenseItem.jsx
+++ client/src/components/Expenses/ExpenseItem.jsx
@@ -13,18 +13,16 @@ const ExpenseItem = (props) => {
 
 	const [title, setTitle] = useState(props.expense.title);
 
-	const changeTitle = () => {
-		setTitle('Updated!');
-	};
+
 
 	return (
+
 		<Card className='expense-item'>
 			<ExpenseDate expense={props.expense} />
 			<div className='expense-item__description'>
 				<h2>{title}</h2>
 				<div className='expense-item__price'>${props.expense.amount}</div>
 			</div>
-			<button onClick={changeTitle}>Change Title</button>
 		</Card>
 	);
 };
