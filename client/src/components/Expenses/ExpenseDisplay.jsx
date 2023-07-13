825fba9f174b1683e87f8b78f97bd1cb725d0773
Matthew Garry <mhgarry92@gmail.com>
2023-07-13 15:58:53 -0400

    pass filtred year

--- client/src/components/Expenses/ExpenseDisplay.jsx
+++ client/src/components/Expenses/ExpenseDisplay.jsx
@@ -1,17 +1,19 @@
+import { useState } from "react";
 import Card from "../Card/Card";
 import "./ExpenseDisplay.css";
 import ExpenseItem from "./ExpenseItem";
 import ExpenseFilter from "./ExpenseFilter";
 
 const ExpenseDisplay = (props) => {
+	const [filteredYear, setFilteredYear] = useState('2020');
+
 	const filterChangeHandler = selectedYear => {
-		console.log('ExpenseDisplay.js');
-		console.log(selectedYear);
+		setFilteredYear(selectedYear);
 	}
 	return (
 		<div>
 			<Card className='expenses'>
-				<ExpenseFilter onChangeFilter={filterChangeHandler} />
+				<ExpenseFilter onChangeFilter={filterChangeHandler} selected={filteredYear} />
 				{props.expense.map((expense) => (
 					<ExpenseItem key={expense.id} expense={expense} />
 				))}
