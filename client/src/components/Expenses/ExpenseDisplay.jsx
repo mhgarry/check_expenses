be26002bab40f886cd3c62b345391347b8a1ae26
Matthew Garry <mhgarry92@gmail.com>
2023-07-14 21:00:03 -0400

    moduralize code further

--- client/src/components/Expenses/ExpenseDisplay.jsx
+++ client/src/components/Expenses/ExpenseDisplay.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 
-import ExpenseItem from './ExpenseItem';
+import ExpensesList from './ExpensesList';
 import Card from '../UI/Card';
 import ExpenseFilter from './ExpenseFilter';
 import './ExpenseDisplay.css';
@@ -16,26 +16,18 @@ const Expenses = (props) => {
 		expense => {
 			return (
 				expense.date.getFullYear().toString() === filteredYear
-			)
+			);
 		}
-	)
+	);
 
 	return (
 		<div>
 			<Card className='expenses'>
-				<ExpenseFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-				{filteredExpenses.map((expense) => {
-					return (
-						<ExpenseItem
-
-							key={expense.id}
-							title={expense.title}
-							amount={expense.amount}
-							date={expense.date}
-						/>
-					);
-				})
-				}
+				<ExpenseFilter
+					selected={filteredYear}
+					onChangeFilter={filterChangeHandler}
+				/>
+				<ExpensesList items={filteredExpenses} />
 			</Card>
 		</div>
 	);
